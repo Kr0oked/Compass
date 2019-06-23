7a009cd9d6aa7bc113c05aaffe6e98334e9d9bd9
Philipp Bobek <philipp.bobek@web.de>
2019-06-23 22:02:38 +0200

    Implement night mode

--- app/src/main/java/com/bobek/compass/MainActivity.kt
+++ app/src/main/java/com/bobek/compass/MainActivity.kt
@@ -9,19 +9,25 @@ import android.hardware.SensorManager
 import android.hardware.SensorManager.SENSOR_DELAY_GAME
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate.*
 import com.bobek.compass.sensor.LowPassFilter
 import com.bobek.compass.sensor.SensorHandler
 import com.bobek.compass.sensor.SensorValues
 import com.bobek.compass.view.Compass
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 private const val SENSOR_SAMPLING_PERIOD_US = SENSOR_DELAY_GAME
 private const val LOW_PASS_FILTER_ALPHA = 0.03f
+private const val STATE_NIGHT_MODE = "night-mode"
 private const val X = 0
 private const val Y = 1
 private const val Z = 2
@@ -36,9 +42,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var accelerometerFilter: LowPassFilter
     private lateinit var magnetometerFilter: LowPassFilter
 
+    private var nightMode: Int = MODE_NIGHT_FOLLOW_SYSTEM
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        initializeNightMode(savedInstanceState)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         compass = findViewById(R.id.compass)
 
@@ -47,6 +57,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorHandler = SensorHandler()
         accelerometerFilter = LowPassFilter(LOW_PASS_FILTER_ALPHA)
         magnetometerFilter = LowPassFilter(LOW_PASS_FILTER_ALPHA)
+    }
+
+    private fun initializeNightMode(savedInstanceState: Bundle?) {
+        nightMode = savedInstanceState?.getInt(STATE_NIGHT_MODE) ?: nightMode
+        setDefaultNightMode(nightMode)
     }
 
     override fun onResume() {
@@ -87,6 +102,47 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         accelerometerFilter.reset()
         magnetometerFilter.reset()
         sensorHandler.reset()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        menu.findItem(R.id.action_night_mode)
+            .setIcon(getNightModeIcon())
+        return true
+    }
+
+    @DrawableRes
+    private fun getNightModeIcon(): Int {
+        return when (nightMode) {
+            MODE_NIGHT_NO -> R.drawable.ic_night_mode_no
+            MODE_NIGHT_YES -> R.drawable.ic_night_mode_yes
+            else -> R.drawable.ic_night_mode_auto
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_night_mode -> {
+                toggleNightMode()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun toggleNightMode() {
+        nightMode = when (nightMode) {
+            MODE_NIGHT_NO -> MODE_NIGHT_YES
+            MODE_NIGHT_YES -> MODE_NIGHT_FOLLOW_SYSTEM
+            else -> MODE_NIGHT_NO
+        }
+
+        recreate()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(STATE_NIGHT_MODE, nightMode)
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
