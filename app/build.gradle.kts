2e4fe00ead38d7f8aef803a80843386de4c2b1f8
Philipp Bobek <philipp.bobek@mailbox.org>
2023-11-04 10:08:27 +0100

    Update to Android 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,3 @@
-/*
- * This file is part of Compass.
- * Copyright (C) 2023 Philipp Bobek <philipp.bobek@mailbox.org>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Compass is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -24,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.bobek.compass"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.bobek.compass"
         minSdk = 22
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 16
         versionName = "1.11.4"
 
@@ -37,25 +19,6 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        resourceConfigurations += listOf(
-            "bg",
-            "bs",
-            "cs",
-            "de",
-            "el",
-            "en",
-            "fi",
-            "fr",
-            "hr",
-            "it",
-            "mk",
-            "nb",
-            "nl",
-            "ru",
-            "sr",
-            "tr",
-            "vi"
-        )
     }
 
     buildTypes {
@@ -77,6 +40,12 @@ android {
     buildFeatures {
         buildConfig = true
         dataBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    androidResources {
+        generateLocaleConfig = true
     }
     testOptions {
         managedDevices {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,3 @@
-/*
- * This file is part of Compass.
- * Copyright (C) 2023 Philipp Bobek <philipp.bobek@mailbox.org>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Compass is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
