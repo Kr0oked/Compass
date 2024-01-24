d2135ac354c6f78dd4d97b5f83e94493388c5f55
Philipp Bobek <philipp.bobek@mailbox.org>
2024-01-24 22:22:39 +0100

    Release 1.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bobek.compass"
         minSdk = 23
         targetSdk = 34
-        versionCode = 18
-        versionName = "1.13.0"
+        versionCode = 19
+        versionName = "1.13.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
