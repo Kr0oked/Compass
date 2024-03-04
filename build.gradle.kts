332df55041faa80023e3e64df9931f2fefb577e5
Philipp Bobek <philipp.bobek@mailbox.org>
2024-03-04 22:05:28 +0100

    Upgrade AGP to 8.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,6 @@
  */
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
